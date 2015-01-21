7e9c3b7ed128cacd9c6d69ca972d966664437556
Nick731 <nickpeters731@gmail.com>
2015-01-21 16:03:27 -0500

    This is my quiz as of 01/21/2014 at 4:03 PM.

--- Quiz/ViewController.m
+++ Quiz/ViewController.m
@@ -10,9 +10,72 @@
 
 @interface ViewController ()
 
+@property (nonatomic) int currentQuestionIndex;
+
+@property (nonatomic, copy) NSArray *questions;
+@property (nonatomic, copy) NSArray *answers;
+
+@property (nonatomic, weak) IBOutlet UILabel *questionLabel;
+@property (nonatomic, weak) IBOutlet UILabel *answerLabel;
+
+
 @end
 
 @implementation ViewController
+
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil
+                          bundle:(NSBundle *)nibBundleOrNil
+{
+    // Call the init method implemented by the superclass
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    
+    if (self) {
+        // Create two arrays filled with questions and answers
+        // and make the pointers point to them
+        
+        self.questions = @[@"What is my name?",
+                           @"What is my major?",
+                           @"Where do I go to school?"];
+        
+        self.answers = @[@"Nicholas Peters",
+                         @"Information Science",
+                         @"University of Pittsburgh"];
+    }
+    
+    // Return the address of the new object
+    return self;
+}
+
+- (IBAction)showQuestion:(id)sender
+{
+    // Step to the next question
+    self.currentQuestionIndex++;
+    
+    // Am I past the last question?
+    if (self.currentQuestionIndex == [self.questions count]) {
+        
+        // Go back to the first question
+        self.currentQuestionIndex = 0;
+    }
+    
+    // Get the string at that index in the questions array
+    NSString *question = self.questions[self.currentQuestionIndex];
+    
+    // Display the string in the question label
+    self.questionLabel.text = question;
+    
+    // Reset the answer label
+    self.answerLabel.text = @"???";
+}
+
+- (IBAction)showAnswer:(id)sender
+{
+    // What is the answer to the current question?
+    NSString *answer = self.answers[self.currentQuestionIndex];
+    
+    // Display it in the answer label
+    self.answerLabel.text = answer;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
